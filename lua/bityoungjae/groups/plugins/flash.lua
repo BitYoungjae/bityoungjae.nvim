35277ce738435ee72db2912d90b906584ebf706e
BitYoungjae <bityoungjae@gmail.com>
2025-12-20 01:10:02 +0900

    style: flash.nvim 하이라이트 색상을 저채도 톤으로 조정
    - 매칭된 영역: 어두운 초록색(#21572C)
    - 점프 레이블: 어두운 붉은색(#6F2B35)
    - 텍스트: 밝은 회색(#D1DDD6)
    - FlashCurrent를 FlashMatch와 같은 색으로 통일하여 단순화

--- lua/bityoungjae/groups/plugins/flash.lua
+++ lua/bityoungjae/groups/plugins/flash.lua
@@ -1,13 +1,25 @@
 -- flash.nvim 하이라이트
 local M = {}
 
+-- Flash 전용 색상 (팔레트와 별도로 눈에 편한 저채도 톤)
+local flash_colors = {
+  match_bg = "#21572C",   -- 매칭된 영역 (어두운 초록색)
+  label_bg = "#6F2B35",   -- 점프 레이블 배경 (어두운 붉은색)
+  label_fg = "#D1DDD6",   -- 텍스트 (밝은 회색)
+}
+
 function M.setup(hl, p)
   -- FlashBackdrop: 배경의 흐릿한 텍스트
   hl(0, "FlashBackdrop", { fg = p.fg_dark })
 
-  -- FlashLabel: 표시되는 점프 레이블
-  -- 마젠타 계열의 keyword (Thistle) 색상을 굵게 강조
-  hl(0, "FlashLabel", { bg = p.keyword, fg = p.fg, bold = true })
+  -- FlashMatch: 매칭된 텍스트 하이라이트
+  hl(0, "FlashMatch", { bg = flash_colors.match_bg, fg = flash_colors.label_fg })
+
+  -- FlashCurrent: 현재 매칭 위치
+  hl(0, "FlashCurrent", { bg = flash_colors.match_bg, fg = flash_colors.label_fg })
+
+  -- FlashLabel: 점프 레이블
+  hl(0, "FlashLabel", { bg = flash_colors.label_bg, fg = flash_colors.label_fg, bold = true })
 end
 
 return M
