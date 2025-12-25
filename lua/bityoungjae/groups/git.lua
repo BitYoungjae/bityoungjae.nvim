c7abcf5610001a36c9c4b03f6d7e383c53f1e888
BitYoungjae <bityoungjae@gmail.com>
2025-12-25 11:04:34 +0900

    fix: snacks.nvim Github PR picker 관련 문제 수정

--- lua/bityoungjae/groups/git.lua
+++ lua/bityoungjae/groups/git.lua
@@ -5,10 +5,10 @@ function M.setup(hl, p)
   local util = require("bityoungjae.util")
 
   -- Diff (30% 불투명도 = 0.3 블렌드)
-  hl(0, "DiffAdd", { bg = util.blend(p.git_add, 0.3, p.bg) })
-  hl(0, "DiffChange", { bg = util.blend(p.git_change, 0.3, p.bg) })
-  hl(0, "DiffDelete", { bg = util.blend(p.git_delete, 0.3, p.bg) })
-  hl(0, "DiffText", { bg = util.blend(p.git_change, 0.5, p.bg) })
+  hl(0, "DiffAdd", { fg = p.git_add, bg = util.blend(p.git_add, 0.3, p.bg) })
+  hl(0, "DiffChange", { fg = p.git_change, bg = util.blend(p.git_change, 0.3, p.bg) })
+  hl(0, "DiffDelete", { fg = p.git_delete, bg = util.blend(p.git_delete, 0.3, p.bg) })
+  hl(0, "DiffText", { fg = p.git_change, bg = util.blend(p.git_change, 0.5, p.bg) })
 
   -- 추가/변경/제거
   hl(0, "Added", { fg = p.git_add })
