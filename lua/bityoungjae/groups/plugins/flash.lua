0a3890e8bed61987e2586ed3e70dd85143cdaf48
bityoungjae <bityoungjae@gmail.com>
2026-09-17 11:09:09 +0900

    feat: FlashLabel 라벨을 type 브라스 칩으로 개선

--- lua/bityoungjae/groups/plugins/flash.lua
+++ lua/bityoungjae/groups/plugins/flash.lua
@@ -7,7 +7,7 @@ function M.setup(hl, p)
   hl(0, "FlashBackdrop", { fg = p.fg_dark })
   hl(0, "FlashMatch", { bg = util.blend(p.string, 0.35, p.bg), fg = p.fg_light })
   hl(0, "FlashCurrent", { bg = util.blend(p.func, 0.40, p.bg), fg = p.fg_light })
-  hl(0, "FlashLabel", { bg = util.blend(p.cursor, 0.55, p.bg), fg = p.bg, bold = true })
+  hl(0, "FlashLabel", { bg = p.type, fg = p.bg, bold = true })
 end
 
 return M
